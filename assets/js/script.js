14df444a94180d8ff1e2bb8323f04d082180ba14
Gordon <gordonjackman@gmail.com>
2023-11-08 19:10:05 -0500

    figured i'd get started.  That's what I got.

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var questionEl = $('#question');
+var choicesEl = $('#choices');
+var timerEl = $('#timer')
+var highScoreEl = $('#high-score');
+var infoEl = $('#info');
+var buttonEl = $('#start')
+var questionArr = [{
+    q: "What does the 'M' in MERN stand for?",
+    a: [{text: "MongoDB", isCorrect: true},
+    {text: "Moo", isCorrect: false},
+    {text: "Moo", isCorrect: false},
+    {text: "Moo", isCorrect: false},
+    ]
+},
+{   q: "What is the capital of Massachusetts?",
+    a: [{text: "New York", is Correct: false},
+    {text: "New York", is Correct: false},
+    {text: "New York", is Correct: false},
+    {text: "Boston", is Correct: true},
+    ]
+
+}
+]
+
+
+$(buttonEl).on('click', function() {
+    
+})
