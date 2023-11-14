6855779b3a88f0f29cbb699898d9ab0de4913cb3
Gordon <gordonjackman@gmail.com>
2023-11-13 23:16:19 -0500

    In order for the scores list to work, a function had to be created that stored initials and scores.  Then a for loop was neccessary to append all the scores. I imagine a table would be better.  Never used a table in html, so I'll work on implementing one later. Most of the styling has been cut short due to time, but it will be completed soon.  Overall it works, but there are a lot of things that need to be cleaned up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var currentQuestion = {};
 var score = 0;
 var availableQuestions = 4;
 var questionCounter = 0;
-
+var existingScores = JSON.parse(localStorage.getItem("scores"))??[];
 // This is my array of questions.
 var questionArr = [{
     q: "What does the 'M' in MERN stand for?",
@@ -47,12 +47,12 @@ var questionArr = [{
 // This on click function starts the timer and sets an alert if time is up.
 $(startEl).on('click', function() {
     $(startEl).css("visibility", "hidden");
-    var counterEl = 75;
+    var counterEl = 60;
+    nextQuestion()
     const timer = setInterval(function() {
     counterEl--;
         if (counterEl >= 0) {
-        timerEl.text('Time: ' + counterEl);
-        startQuestion();
+        timerEl.text('Time: ' + counterEl)
     }   else {
         alert('Time is up!');
         clearInterval(counterEl);
@@ -60,29 +60,14 @@ $(startEl).on('click', function() {
     }
 }, 1000)
 
-// If you run out of questions in the array, or you run out of time, 
-// the game stops.  Then the score is logged into localstorage to be move to the high scores.
-function startQuestion() {
-    if (availableQuestions === 0 || counterEl === 0) {
-        localStorage.setItem('lastScore', score);
-        $(nextEl).css("visibility", "hidden");
-        postScore();
-        clearInterval(timer);
-        return;
-    }   else {
-        nextQuestion();
-    }
-    
-}
 
 // This function replaces "Quiz Game" with the actual question.
 // Then it creates buttons that have potential answers on them.
 function nextQuestion() {
-    $(nextEl).css("visibility", "hidden");
     currentQuestion = questionArr[questionCounter];
     let questionNumber = questionCounter + 1;
     $(questionEl).text(questionNumber + ". " + currentQuestion.q);
-
+    $(choicesEl).html('');
     currentQuestion.a.forEach(a => {
         var button = document.createElement("button");
         $(button).text(a.text);
@@ -95,10 +80,10 @@ function nextQuestion() {
         $(button).on("click", guess);
     });
     $(choicesEl).css("visibility", "visible");
-    
+}   
     
 // When you click a potential answer, this function adds a class to the potential answers
-// corresponding to their correctness.  The colors will change to reflect that.
+// corresponding to their correctness.  In the interest of time I have neglected to style the anwer buttons.
 function guess(event) {
     var answer = event.target;
     var rightAnswer = answer.dataset.isCorrect === "true";
@@ -113,25 +98,39 @@ function guess(event) {
         $(info).css("visibility", "visible");
         counterEl -= 10;
     }
-        
-    $(nextEl).css("visibility", "visible");
     questionCounter++;
-    availableQuestions--;
-    $(nextEl).on("click", nextQuestion)
-    // console.log(questionCounter);
-    // console.log(availableQuestions);
+    postScore();
 }
-}   
-
+ 
+// This function was created to stop the game at the conclusion of the questions and save user score.
 function postScore() {
-    var initials = prompt("You Scored: " + score + " points! Enter your initials to save your score.");
-    localStorage.setItem("initials", initials);
-    var highArray = [{}]
-    
+    if (questionCounter === 4 || counterEl === 0) {
+        clearInterval(timer);
+        saveScore();
+        return;
+    }   else {
+        nextQuestion();
+    } 
+}
 
+// This function creates a var in local storage that stores user initials and score.
+// That var is then used to populate the scores list.  
+function saveScore() {
+    var initials = prompt("You Scored: " + score + " points! Enter your initials to save your score.");
+    var existingScores = JSON.parse(localStorage.getItem("scores"))??[];
+    var newScores = existingScores.concat(
+        [{  initials,
+            score
+        }
+        ]
+    );
+    localStorage.setItem("scores", JSON.stringify(newScores));
+    for (var i = 0; i < newScores.length; i++) {
+        var txt = $("<p></p>").text(newScores[i].initials + '.............................' + newScores[i].score);
+        $(highScoreEl).append(txt);
+
+    }
+    }
 
 }
-})
-
-
-
+)
